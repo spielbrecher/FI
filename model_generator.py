202986bfeca9b5e54f5be214eee7c891cef65948
spielbrecher <jagello@list.ru>
2021-05-09 22:17:16 +0900

    Predictive models builder for investors

--- model_generator.py
+++ model_generator.py
@@ -1,8 +1,30 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.neural_network import MLPRegressor
+from sklearn.preprocessing import StandardScaler
+
 
 class ModelGenerator:
-    pass
+
+    def __init__(self):
+        self.linear_regression_models = []  # List of linear regressions
+        self.random_forest_regression_models = []  # List of random forest regressions
+        self.mlp_models = []  # List of MLP models
+
+        self.financials = pd.DataFrame()  # financials
+
+    def set_financials(self, financials: pd.DataFrame):
+        self.financials = financials
+
+    def get_financials(self):
+        return self.financials
+
+    def build_linear_regression_model(self):
+        pass
+
+    def build_linear_regression_models_for_all_features(self):
+        pass
 
